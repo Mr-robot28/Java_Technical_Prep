d3bb2576c8087fe9bfe9cb773c9505d4b55a7a45
Sumedh Hajare <hajaresumedh1000@gmail.com>
2026-07-14 09:49:29 +0530

    L1_Q4 and Q5

---
+++ src/com/code/L1_Q4.java
@@ -0,0 +1,16 @@
+package com.code;
+
+// Write a program to read the radius of a circle and print its area and circumference.
+public class L1_Q4 {
+
+	public static void main(String[] args) {
+		
+		int r = 10;
+		double pi = 3.142;
+		
+		System.out.println("Area of circle : " + (pi * (r * r)));
+		
+		System.out.println("Circumference of circle : " + (2 * pi * r));
+		
+	}
+}
